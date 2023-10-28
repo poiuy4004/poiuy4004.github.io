78f5db5da5235e898e6b6594251e77bbc60573a3
poiuy4004 <poiuy4004@naver.com>
2023-10-28 15:01:42 +0900

    skill

--- src/assets/skills.js
+++ src/assets/skills.js
@@ -2,40 +2,40 @@
 const skills = {
   main: [
     {
-      name: "HTML",
-      level: "85%",
-    },
-    {
-      name: "CSS",
-      level: "85%",
-    },
-    {
-      name: window.innerWidth>768? "Javascript" : "JS",
-      level: "95%",
+      name: "React",
+      level: "90%",
     },
     {
       name: window.innerWidth>768? "Typescript" : "TS",
       level: "80%",
     },
     {
-      name: "React",
-      level: "90%",
+      name: window.innerWidth>768? "Javascript" : "JS",
+      level: "95%",
     },
     {
       name: "Next.js",
       level: "60%",
     },
     {
-      name: "jQuery",
-      level: "40%",
+      name: "AWS",
+      level: "60%",
+    },
+    {
+      name: "MySQL",
+      level: "30%",
     },
     {
       name: "git",
       level: "80%",
     },
     {
-      name: "MySQL",
-      level: "30%",
+      name: "HTML",
+      level: "85%",
+    },
+    {
+      name: "CSS",
+      level: "85%",
     },
   ],
   others: ["github","Figma","Postman","OAuth","StoryBook","Photoshop","PremierePro"]
